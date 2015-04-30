ea5ef92d3a49c49ab1cfe4497f370fb37cc9b8a8
Haleigh Harper <haleighharper@Haleighs-MBP.fios-router.home>
2015-04-29 21:50:57 -0400

    added event listeners for buttons

--- timers.js
+++ timers.js
@@ -1,3 +1,5 @@
+var intervalID = setInterval(startTimer, 1000)
+var time = 0
 var reset = document.querySelector("#reset")
 var start = document.querySelector("#start")
 var pause = document.querySelector("#pause")
@@ -10,20 +12,30 @@ var timer = document.querySelector("#timer")
 //use clearInterval for pause
 
 // * When "Start" is clicked, the text "Stop Watch" should be replaced by "Time elapsed: 0", and the count should increment every second.
-  //When "Start" button is clicked, the text "Stop Watch" changes to "Time elapsed: 0"
-  start.addEventListener("click", function(event){
-    //replace "stop watch" to "time elapsed:0"
-    timer.innerHTML = "Time elapsed: 0"
-    //and the count should increment every second
+
+  function startTimer(){
+    timer.innerHTML = time; time++;
   }
-  )
+//When "Start" button is clicked, the text "Stop Watch" changes to "Time elapsed: 0"
+start.addEventListener("click", function(event){
+    //replace "stop watch" to "time elapsed:0"
+    //var intervalID = setInterval(startTimer, 1000)
+    setInterval(startTimer, 1000);
+
+    //and the count should increment every second
+  })
+
 
 // * When "Reset" is clicked, the text should return to "Stop Watch"
 reset.addEventListener("click", function(event){
-  timer.innerHTML = "Time elapsed: 0"
+  clearInterval(intervalID);
+  time = 0
+  //timer.innerHTML = "Time elapsed: 0"
+var intervalID = setInterval(startTimer, 1000);
+  //intervalID
 })
 
 // * When "Pause" is clicked, the text should say "Time elapsed: 1", but stop incrementing.
 pause.addEventListener("click", function(event){
-  clearInterval
+  clearInterval(intervalID);
 })
